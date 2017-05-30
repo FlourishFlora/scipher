c3e82bac023a7d7ba600f6d256c44c489028eec9
FlourishFlora <flamingdescent@gmail.com>
2017-05-29 23:48:37 -0400

    Update random ad every 5 seconds

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 var interval = undefined;
-function requestText(arg) {
-    $.get("/generate", {arg:arg}, updateOriginal);
+function requestText(arg, callback) {
+    $.get("/generate", {arg:arg}, callback);
 }
+function request
 function updateOriginal(newText) {
     $("#orig-text").val(newText);
     console.log(newText);
@@ -11,6 +12,9 @@ function updateClosestMatch(newText) {
     console.log(newText);
 }
 $(document).ready(function() {
+    interval = setInterval(function() {
+        requestText($("#secret-message").val(), updateClosestMatch)
+    }, 5000);
     $(".submit").click(function (e) {
         $.get("/generate", {}, function (response) {
             $("#latest").empty();
@@ -20,7 +24,7 @@ $(document).ready(function() {
         var c = $(this);
         $.when(
         c.focusout()).then(function() {
-            requestText($("#secret-message").val());
+            requestText($("#secret-message").val(), updateOriginal)
         });
     });
 })

45579246908c2cd1e1cc8ecd8cafdd8319c57ef2
FlourishFlora <flamingdescent@gmail.com>
2017-05-29 22:26:00 -0400

    Update original ad from editing secret message
    Also start to add queue stuff

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import subprocess
 from flask import Flask, render_template, redirect, url_for, session, request
+from Queue import Queue
+from threading import Thread
 app = Flask(__name__)
 random_text = ""
+buffer = Queue(maxsize = 100)
 
 @app.route("/")
 def home():
@@ -15,6 +18,14 @@ def get_random_text(string):
     output = subprocess.check_output(["./encode.py"], stdin = pipe.stdout)
     return output
 
+def worker():
+    return
+
 if __name__ == "__main__":
     random_text = get_random_text("hey")
     app.run(debug = True)
+    worker_thread = Thread(target = worker)
+    worker_thread.setDaemon(True)
+    #worker_thread.start()
+    #buffer = [get_random_text("aye aye captain") for i in range(0, 5)]
+    #print(buffer)
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,21 @@
+function requestText(arg) {
+    $.get("/generate", {arg:arg}, updateOriginal);
+}
+function updateOriginal(newText) {
+    $("#orig-text").val(newText);
+    console.log(newText);
+}
 $(document).ready(function() {
     $(".submit").click(function (e) {
         $.get("/generate", {}, function (response) {
             $("#latest").empty();
             $("#latest").append(response);})
+    });
+    $("#secret-message").change(function() {
+        var c = $(this);
+        $.when(
+        c.focusout()).then(function() {
+            requestText($("#secret-message").val());
+        });
     });
 })
